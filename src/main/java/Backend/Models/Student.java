0bec7489142dd52f2e4c1f57c8451904942952ba
M.Walaa <mwalaa.spider@gmail.com>
2025-11-16 18:51:29 +0200

    Add enrollment and lesson tracking to Student class

--- src/main/java/Backend/Models/Student.java
+++ src/main/java/Backend/Models/Student.java
@@ -19,4 +19,80 @@ import java.util.HashMap;
 
 public class Student extends User {
     
+    
+    private ArrayList<Integer> enrolledCourseIds = new ArrayList<>(); //id list
+
+    private HashMap<Integer, ArrayList<Integer>> progress = new HashMap<>(); //map
+
+    public Student(int id, String username, String email, String passwordHash){
+        super(id, "STUDENT", username, email, passwordHash);
+    }
+
+
+    public boolean enrollCourse(Course course) {
+
+        int courseId = course.getCourseId();
+
+        if(enrolledCourseIds.contains(courseId)) {
+            System.out.println("Already enrolled in course " + courseId);
+            return false;   //already enrolled
+        }
+
+        enrolledCourseIds.add(courseId);
+
+        progress.put(courseId, new ArrayList<>());
+
+        course.enroll(this.getId());
+
+        System.out.println("Student " + this.getId() + " successfully enrolled in Course " + courseId);
+        return true;
+    }
+
+
+    public boolean markLessonCompleted(int courseId, int lessonId){
+
+        if (!enrolledCourseIds.contains(courseId)) {
+            System.out.println("Student is not enrolled in this course!");
+            return false;  //student not enrolled
+        }
+
+        ArrayList<Integer> completedLessons = progress.get(courseId);
+
+        if (completedLessons == null) {
+            completedLessons = new ArrayList<>();
+            progress.put(courseId, completedLessons);
+        }
+
+        
+        if (completedLessons.contains(lessonId)) {
+            System.out.println("Lesson already completed.");
+            return false;   //lesson already done
+        }
+
+        //mark lesson complete
+        completedLessons.add(lessonId);
+
+        System.out.println("Marked lesson " + lessonId + " completed for course " + courseId);
+        return true;
+    }
+
+    
+    
+    
+    
+
+    public ArrayList<Integer> getCompletedLessons(int courseId) {
+        return progress.getOrDefault(courseId, new ArrayList<>());
+    }
+
+    public ArrayList<Integer> getEnrolledCourseIds() {
+        return enrolledCourseIds;
+    }
+
+
+    //for gui
+    @Override
+    public void openDashboard(JFrame parentFrame) {
+    }
+    
 }
