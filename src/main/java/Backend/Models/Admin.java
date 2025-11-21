0483dadfe416486e5bb8b5a7e310b539d17c1859
PolaNasser13 <gjfcdg1@gmail.com>
2025-11-22 00:32:25 +0200

    add some atrributes and methods in course and admin

--- src/main/java/Backend/Models/Admin.java
+++ src/main/java/Backend/Models/Admin.java
@@ -22,20 +22,7 @@ public class Admin extends User {
     public String getRole() {
         return "admin";
     }
-
-    public void approveCourse(Course course) {
-        course.setApprovalStatus(ApprovalStatus.APPROVED);
-        course.addAudit("APPROVED",this.getUserId(),Instant.now().toString());
-    }
-    
-    
-    
-    public void rejectCourse(Course course, String reason) {
-        course.setApprovalStatus(ApprovalStatus.REJECTED);
-        course.addAudit("REJECTED: " + reason,this.getUserId(),Instant.now().toString());
-    }
-    
-   
+ 
     @Override
     public JSONObject toJSON() {
         return super.toJSON();
--- src/main/java/Backend/Models/ApprovalStatus.java
+++ src/main/java/Backend/Models/ApprovalStatus.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package courses;
+package Backend.Models;
 
 /**
  *
@@ -11,8 +11,7 @@ package courses;
 public enum ApprovalStatus {
     PENDING,
     APPROVED,
-    REJECTED,
-    
+    REJECTED
 }
 
 
