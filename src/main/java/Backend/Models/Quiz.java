47b9810e6b55fa2e77d096e5f91f879509679172
Youssef hesham <capoyoussef16@gmail.com>
2025-11-22 17:36:41 +0200

    Add Quiz

--- src/main/java/Backend/Models/Quiz.java
+++ src/main/java/Backend/Models/Quiz.java
@@ -4,10 +4,86 @@
  */
 package Backend.Models;
 
+import java.util.ArrayList;
+import courses.Question;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  *
- * @author HP_Laptop
+ * @author Pc
  */
 public class Quiz {
+    private ArrayList<Question> questions;
+    private int quizId;
+    private int passingScore;
+    public Quiz(int quizId,int passingScore){
+        setQuizId(quizId);
+        setPassingScore(passingScore);
+        this.questions=new ArrayList<>();
+        
+    }
+    public Quiz(JSONObject json){
+        this.quizId=json.getInt("quizId");
+        this.passingScore=json.getInt("passingScore");
+        this.questions=new ArrayList<>();
+        JSONArray questionArr=json.optJSONArray("question");
+        if(questionArr!=null){
+            for(int i=0;i<questionArr.length();i++){
+          questions.add(new Question(questionArr.getJSONArray(i));
+                  }
+    }
+        
+    }
+    public boolean addQuestion(Question q){
+    for(int i=0;i<questions.size();i++){
+    if(questions.get(i).getQuestionId()==q.getQuestionId()){
+        return false;
+    }
     
+       
+}
+    questions.add(q);
+        return true;
+}
+    public boolean isPassed(int score){
+        if(score>=passingScore){
+        return true;
+        }
+        return false;
+    }
+    
+    public JSONObject toJSON(){
+        JSONObject obj=new JSONObject();
+        obj.put("quizId", quizId);
+        obj.put("passingScore", passingScore);
+        JSONArray questionArr=new JSONArray();
+        for(int i=0;i<questions.size();i++){
+            questionArr.put(questions.get(i).toJson());
+        }
+        obj.put("question", questionArr);
+        return obj;
+    }
+    public int getQuizId() {
+        return quizId;
+    }
+
+    public int getPassingScore() {
+        return passingScore;
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+    public void setQuizId(int quizId){
+        if(quizId<0){
+        throw new IllegalArgumentException("quizId must be greater than 0");}
+        this.quizId=quizId;
+    }
+    public void setPassingScore(int passingScore){
+    if(passingScore<0){
+        throw new IllegalArgumentException("passingScore must be greater than 0");
+    }  
+    this.passingScore=passingScore;
+    }
 }
